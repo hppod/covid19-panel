ae69cb16f6eb897fd7601d9e5441ed509692d69c
hppod <hppodgornik@gmail.com>
2020-06-07 17:07:40 -0300

    Remove / on server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     }
 })
 
-app.use(express.static(__dirname + '/dist/covid19-panel'))
+app.use(express.static(__dirname + 'dist/covid19-panel'))
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
 })
 
 app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/dist/covid19-panel/index.html'))
+    res.sendFile(path.join(__dirname + 'dist/covid19-panel/index.html'))
 })
 
 app.listen(process.env.PORT || 8080)
