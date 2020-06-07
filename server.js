8fca4548fe6f4168e6c5da104db0621cf354b2ad
hppod <hppodgornik@gmail.com>
2020-06-07 16:01:13 -0300

    Install express and configure server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use((req, res, next) => {
+    if (req.headers["x-forwarded-proto"] == "http") {
+        res.redirect(`https://${req.headers.host}${req.url}`)
+    } else {
+        next()
+    }
+})
+
+app.use(express.static(__dirname + '/dist/covid19-panel'))
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    next()
+})
+
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/dist/covid19-panel/index.html'))
+})
+
+app.listen(process.env.PORT || 8080)
